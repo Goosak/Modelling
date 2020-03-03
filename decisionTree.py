a09e173f5fb21e29060229772ebabf6e71a1cddf
Vladimir <azov-97@mail.ru>
2020-03-03 10:09:07 +0700

    some additions

---
+++ decisionTree.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 10:06:22 2020
+
+@author: Vladimir
+"""
+
--- regression.py
+++ regression.py
@@ -8,6 +8,12 @@ Created on Tue Feb 18 13:28:57 2020
 import numpy as np
 import numpy.random as rand
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def K(x, xi, cs):
+    if abs(x-xi)<=cs:
+        return 3/4*(1-((x-xi)**2/cs))
+    return 0
 
 minX = -100
 maxX = 100
@@ -16,13 +22,19 @@ x2 = np.array([i for i in range(minX, maxX)])
 x1 = x1/50.0
 x2 = x2/50.0
 y = []
+ykt=[]
 for i in range(0,len(x1)):    
     y.append((x1[i]+x2[i])**2+(rand.random()/2-0.5))
     #y.append((x1[i]+x2[i])**2+(rand.random()/2-0.5))
 
-fig = plt.figure()
+
+
+fig = plt.figure(0)
 ax = fig.add_subplot(111,projection="3d")
-ax.scatter(xs=x1, ys=x2, zs=y)   
+ax.scatter(xs=x1, ys=x2, zs=y)  
+
+#plt.figure(1)
+ 
 
 print("Выборка:")
 print("x1||x2||y")
